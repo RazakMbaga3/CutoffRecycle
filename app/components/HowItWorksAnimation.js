ac9ac65d9666b2ecd7ab7ba642e55620a77ee04b
eitanschreiber123 <eitanschreiber97@gmail.com>
2025-07-11 14:16:11 -0500

    new images

--- app/components/HowItWorksAnimation.js
+++ app/components/HowItWorksAnimation.js
@@ -22,7 +22,7 @@ export default function HowItWorksAnimation() {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
         </svg>
       ),
-      image: "/Images/Photos/IMG_20241021_121410_339.jpg"
+      image: "/new/people.png"
     },
     {
       title: "Processing",
@@ -32,7 +32,7 @@ export default function HowItWorksAnimation() {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
         </svg>
       ),
-      image: "/Images/Photos/IMG_20241105_143019_017.jpg"
+      image: "/new/science.png"
     },
     {
       title: "Conversion",
@@ -53,7 +53,7 @@ export default function HowItWorksAnimation() {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
       ),
-      image: "/Images/Photos/IMG_20241105_143510_986.jpg"
+      image: "/new/farm.png"
     },
     {
       title: "Growth",
