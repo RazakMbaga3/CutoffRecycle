d4a17873062a5cea6899af4096bdda646242f3ed
RazakMbaga3 <razakmbaga3@gmail.com>
2025-08-18 12:00:03 +0300

    feat: Update API routes, add dependencies, and new assets

--- app/api/employee/route.js
+++ app/api/employee/route.js
@@ -2,17 +2,20 @@ import connectDB from "../../../lib/mongodb";
 import mongoose from "mongoose";
 import {EmployeeType, ActiveEmployeeType} from '../../models'
 
-export async function GET(req, res) {
+export async function GET(req) {
   try {
     await connectDB(); // Ensure Mongoose connection
 
     // Fetch all documents from the 'usertypes' collection using Mongoose
     const users = await EmployeeType.find({}).lean(); 
 
-    return Response.json(users.length ? users : [], { status: 200 });
+    return Response.json(users.length ? users : []);
   } catch (error) {
     console.error("Database error:", error);
-    res.status(500).json({ error: "Internal Server Error", details: error.message });
+    return Response.json(
+      { error: "Internal Server Error", details: error.message },
+      { status: 500 }
+    );
   }
 }
 
